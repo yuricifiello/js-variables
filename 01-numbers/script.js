bd9bd9683fb312d31cf46ace7fc9d0ae1d104803
yuricifiello <yuricifiello@gmail.com>
2025-03-31 15:23:11 +0200

    numbers-2

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -10,7 +10,7 @@ const numOnePlusNumTwo = numOne + numTwo;
 console.log(numOnePlusNumTwo); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
-const numThreeTimesNumTwo = NaN;
+const numThreeTimesNumTwo = numThree * numTwo;
 console.log(numThreeTimesNumTwo); // Output: 512
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
