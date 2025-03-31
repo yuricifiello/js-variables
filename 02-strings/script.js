c15d630e2cc02219e56ffda32c400aa1ca189b49
yuricifiello <yuricifiello@gmail.com>
2025-03-31 15:50:22 +0200

    strings-2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,13 +1,19 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
-let nome = "Yuri";
+let name = "Yuri";
 // Stampa il valore di nome nella console.
-console.log("Yuri");
+console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
+let number = 1998;
+let text = "hello world";
+let bool = true;
 // Stampa tutti i valori nella console.
+console.log(number);
+console.log(text);
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente
