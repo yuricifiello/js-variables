cd39995567c69953d38cda72ec6ea62bd14c9247
yuricifiello <yuricifiello@gmail.com>
2025-03-31 16:51:48 +0200

    strings -4

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -27,5 +27,5 @@ console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
