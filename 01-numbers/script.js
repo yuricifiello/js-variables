70f2a19945e8a305007b3ef009cb22391bfc1e07
yuricifiello <yuricifiello@gmail.com>
2025-03-31 15:22:25 +0200

    numbers-1

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -6,7 +6,7 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // gli operatori numerici di Javascript
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = NaN;
+const numOnePlusNumTwo = numOne + numTwo;
 console.log(numOnePlusNumTwo); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
